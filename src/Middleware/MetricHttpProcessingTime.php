7906e395b58ca334128c9d4b9541701ba5629529
Lasse Rafn <lasserafn@gmail.com>
2020-11-23 16:40:49 +0000

    Better tracking

--- src/Middleware/MetricHttpProcessingTime.php
+++ src/Middleware/MetricHttpProcessingTime.php
@@ -8,13 +8,18 @@ use OurMetrics\SDK\Models\Unit;
 class MetricHttpProcessingTime
 {
 	/**
-	 * @param Request  $request
-	 * @param \Closure $next
+	 * @param Request     $request
+	 * @param \Closure    $next
+	 * @param boolean     $trackUser (include user id in tags?)
+	 * @param null|string $grouping  (NULL, 'endpoint' or 'controller')
 	 *
 	 * @return mixed
 	 */
-	public function handle( $request, \Closure $next ) {
+	public function handle( $request, \Closure $next, $trackUser = true, $grouping = 'endpoint' )
+	{
 		$request->attributes->set( '_ourMetrics_start', microtime( true ) );
+		$request->attributes->set( '_ourMetrics_trackUser', $trackUser );
+		$request->attributes->set( '_ourMetrics_grouping', $grouping );
 
 		return $next( $request );
 	}
@@ -25,16 +30,27 @@ class MetricHttpProcessingTime
 	 *
 	 * @throws \Exception
 	 */
-	public function terminate( Request $request, $response ) {
+	public function terminate( Request $request, $response )
+	{
+		$tags = [];
+
+		if ( $request->attributes->get( '_ourMetrics_trackUser', false ) ) {
+			$tags['user'] = optional( $request->user() )->id ?? 'guest';
+		}
+
+		if ( $grouping = $request->attributes->get( '_ourMetrics_grouping' ) ) {
+			if ( $grouping === 'controller' ) {
+				$tags['controller'] = optional( $request->route() )->getAction() ?? 'Unknown controller';
+			} elseif ( $grouping === 'endpoint' ) {
+				$tags['endpoint'] = $request->method() . ' ' . ( optional( $request->route() )->uri() ?? '/' );
+			}
+		}
+
 		OurMetrics::dispatch( new Metric(
 			'HTTP Processing Time',
 			microtime( true ) - ( defined( 'LARAVEL_START' ) ? LARAVEL_START : $request->attributes->get( '_ourMetrics_start' ) ),
 			Unit::SECONDS,
-			[
-				'user'     => optional( $request->user() )->id ?? 'guest',
-				'endpoint' => $request->method() . ' ' . ( optional( $request->route() )->uri() ?? '/' ),
-			],
-			new \DateTime()
+			$tags
 		) );
 	}
 }
